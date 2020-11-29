daa96f938dc451a3bc1a3471b57ab31351e6fe78
Tobias Nett <skaldarnar@googlemail.com>
2020-11-30 00:09:18 +0100

    chore(ItemsCategory): manage and sort item help entries via custom data type (#12)
    Co-authored-by: Josephine Rueckert <jd.rueckert@googlemail.com>

---
+++ src/main/java/org/terasology/inGameHelp/helpCategories/ItemHelpEntry.java
@@ -0,0 +1,30 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.inGameHelp.helpCategories;
+
+import org.terasology.rendering.nui.widgets.browser.data.DocumentData;
+
+class ItemHelpEntry {
+    private final String hyperlink;
+    private final String displayName;
+    private final DocumentData documentData;
+
+    ItemHelpEntry(String hyperlink, String displayName, DocumentData documentData) {
+        this.hyperlink = hyperlink;
+        this.displayName = displayName;
+        this.documentData = documentData;
+    }
+
+    String getHyperlink() {
+        return hyperlink;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+
+    DocumentData getDocumentData() {
+        return documentData;
+    }
+}
