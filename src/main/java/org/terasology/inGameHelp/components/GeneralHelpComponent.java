3e163dd1c066205bb8c97c6474aed5a499f3acdd
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 19:00:01 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#13)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/inGameHelp/components/GeneralHelpComponent.java
+++ src/main/java/org/terasology/inGameHelp/components/GeneralHelpComponent.java
@@ -1,24 +1,11 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.inGameHelp.components;
 
 import com.google.common.collect.Lists;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.rendering.nui.widgets.browser.data.ParagraphData;
 import org.terasology.engine.rendering.nui.widgets.browser.data.basic.HTMLLikeParser;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.inGameHelpAPI.components.HelpItem;
 
 import java.util.ArrayList;
@@ -27,11 +14,11 @@ import java.util.List;
 /**
  * Use this to add a subsection of help to the general category.
  */
-public class GeneralHelpComponent implements Component, HelpItem {
+public class GeneralHelpComponent implements Component<GeneralHelpComponent>, HelpItem {
     /** Title of this component. */
     public String title;
 
-    /** Name of this help category. */ 
+    /** Name of this help category. */
     public String category = "General";
 
     /** Description of this help item. */
@@ -65,5 +52,13 @@ public class GeneralHelpComponent implements Component, HelpItem {
             result.add(HTMLLikeParser.parseHTMLLikeParagraph(null, paragraph));
         }
         return result;
+    }
+
+    @Override
+    public void copyFrom(GeneralHelpComponent other) {
+        this.title = other.title;
+        this.category = other.category;
+        this.paragraphText.clear();
+        this.paragraphText.addAll(other.paragraphText);
     }
 }
