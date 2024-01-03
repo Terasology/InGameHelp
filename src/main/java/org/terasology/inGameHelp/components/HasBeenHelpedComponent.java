0cb71f6f6dbc1c8de3cf31bf45cbeda87244a9ba
Zangus <wong.thi@gmail.com>
2024-01-03 19:26:37 +0100

    refactor: Components without attributes to EmptyComponent (#15)
    Co-authored-by: thierrywong <wong.t@sfeir.com>

--- src/main/java/org/terasology/inGameHelp/components/HasBeenHelpedComponent.java
+++ src/main/java/org/terasology/inGameHelp/components/HasBeenHelpedComponent.java
@@ -2,15 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.inGameHelp.components;
 
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 /**
  * Component for when the user has been helped.
  * This component is added to the entity whenever the user presses the button that displays the InGameHelpScreen, so that another screen doesn't display if that particular user is currently being helped.
  */
-public class HasBeenHelpedComponent implements Component<HasBeenHelpedComponent> {
-    @Override
-    public void copyFrom(HasBeenHelpedComponent other) {
-
-    }
+public class HasBeenHelpedComponent extends EmptyComponent<HasBeenHelpedComponent> {
 }
